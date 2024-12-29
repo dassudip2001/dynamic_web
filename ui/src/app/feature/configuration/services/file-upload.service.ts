0b7c841e87865ab77eef1d0cf8303963f9ced5d6
Sudip Das <sudip5428@gmail.com>
2024-12-29 22:21:42 +0530

    code commit for file upload functionality add

--- ui/src/app/feature/configuration/services/file-upload.service.ts
+++ ui/src/app/feature/configuration/services/file-upload.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FileUploadService {
+  private readonly BASE_URL = 'http://localhost:3000/api/v1';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  uploadFile(file: File) {
+    const formData = new FormData();
+    formData.append('file', file);
+    return this.http
+      .post(`${this.BASE_URL}/upload`, formData)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: any) {
+    console.error('File upload error:', error);
+    return throwError(error);
+  }
 }
--- ui/src/app/feature/configuration/views/our-work/our-work.component.ts
+++ ui/src/app/feature/configuration/views/our-work/our-work.component.ts
@@ -1,4 +1,36 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { FileUploadService } from '../../services/file-upload.service';
+import { OurWorkService } from '../../services/our-work.service';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+export interface UploadResponse {
+  uploadResult: UploadResult;
+  optimizeUrl: string;
+  autoCropUrl: string;
+}
+
+export interface UploadResult {
+  asset_id: string;
+  public_id: string;
+  version: number;
+  version_id: string;
+  signature: string;
+  width: number;
+  height: number;
+  format: string;
+  resource_type: string;
+  created_at: string;
+  tags: any[];
+  bytes: number;
+  type: string;
+  etag: string;
+  placeholder: boolean;
+  url: string;
+  secure_url: string;
+  asset_folder: string;
+  display_name: string;
+  original_filename: string;
+  api_key: string;
+}
 
 @Component({
   selector: 'app-our-work',
@@ -10,9 +42,39 @@ export class OurWorkComponent {
   @Input() isOpened: boolean = false;
   @Input() title: string = '';
   @Output() modalClose = new EventEmitter<void>();
+  previewUrl!: UploadResponse;
+
+  #_fileUploadService = inject(FileUploadService);
+  #ourWorkService = inject(OurWorkService);
+  #_fb = inject(FormBuilder);
+
+  ourWorkForm = this.#_fb.group({
+    title: new FormControl('', Validators.required),
+    subtitle: new FormControl(''),
+    description: new FormControl(''),
+    image_url: new FormControl(''),
+  });
 
   action: string = 'Add'; // Add or Edit
   onClose() {
     this.modalClose.emit();
   }
+
+  onFileSelected(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const file = target.files?.[0];
+    console.log(file);
+    if (file) {
+      this.#uploadFile(file);
+    }
+  }
+
+  #uploadFile(file: File) {
+    this.#_fileUploadService.uploadFile(file).subscribe((res) => {
+      console.log(res);
+      this.previewUrl = res as UploadResponse;
+    });
+  }
+
+  #createWork() {}
 }
